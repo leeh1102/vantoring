a07bb9c7552851576ccf0e917540a85139bf3ea9
Heather Lee <leeh1102@gmail.com>
2023-04-06 13:21:50 -0700

    Setup backend with express and mongoDB

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+var http = require("http").Server(app);
+
+const path = require("path");
+const port = process.env.PORT || 8080;
+const mongoose = require("mongoose")
+app.use(express.json());
+
+// Connect to the port and database
+if (process.env.NODE_ENV === "production")
+{
+    app.use(express.static('build'));
+    app.get('*', (req, res) =>
+    {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    })
+}
+
+app.listen(port, async (err) =>
+{
+    if (err) return console.loge(err);
+    try
+    {
+        await mongoose.connect(
+            "mongodb + srv://root:ayG2wQLoY1GRo0GS@vantoring-be.ahjfgzl.mongodb.net/?retryWrites=true&w=majority"
+        );
+    } catch (error)
+    {
+        console.log("db error");
+    }
+    console.log("Server running on port: ", port);
+})
+
+
