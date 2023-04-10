eede15faaf2d49f99254af65db941a4badf51864
Gyeongmin Lee <gminlee.316@gmail.com>
2023-04-10 14:08:26 -0700

    create QuestionCard component - use react-query and use SSR

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,17 +1,23 @@
 import { Container, Heading, Text } from "@chakra-ui/layout";
 import Head from "next/head";
 import AllQuestions from "../components/AllQuestions";
+import api from "../services/api";
 
-export default function Home() {
+export async function getServerSideProps() {
+  const questions = await api.getQuestions();
+  return {
+    props: { questions },
+  };
+}
+
+export default function Home({ questions }) {
   return (
     <>
       <Head>
         <title>Vantoring</title>
       </Head>
       <Container p={10} maxW="none">
-        <Heading mb={3}>Vantoring Application</Heading>
-        <Text>환영합니다</Text>
-        <AllQuestions />
+        <AllQuestions initialQuestions={questions} />
       </Container>
     </>
   );
